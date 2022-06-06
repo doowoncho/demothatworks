388acff2d645dc4d2e628dd810e9bc1d4adc2e69
doowoncho <84430881+doowoncho@users.noreply.github.com>
2022-06-06 15:44:20 +0000

    created mad libs

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 name = input("gimme a name: ")
 adjective = input("gimme an adjective: ")
 noun = input("gimme a noun: ")
+verb = input("verb: ")
+holiday = input("gimme a holiday: ")
 
-print(f"")
+print(f"hi my name is {name} I like to spend my {adjective} nights at the {noun} where I will often be caught {verb} while I look forward to {holiday} because that is my favourite time of year")
